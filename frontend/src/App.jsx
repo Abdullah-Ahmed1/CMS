72bb39f86becc4914e6d0966a24d6874adcbeebf
Abdullah Ahmed <abdullah.ahmed@visionx.io>
2023-03-08 12:51:34 +0500

    routes added + leave management page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import HomePage from './pages/HomePage'
+import LeaveManagementPage from './pages/LeaveManagementPage'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+        <Routes>
+        <Route path='/'  element = {<HomePage/>}/>
+        <Route path='/leave-management'  element = {<LeaveManagementPage/>}/>
+        </Routes>
+      </Router>
+     
     </div>
   )
 }
---
+++ frontend/src/pages/LeaveManagementPage.jsx
@@ -0,0 +1,8 @@
+const LeaveManagementPage = ()=>{
+    return(
+        <div>
+            <h2> Leave Management Page</h2>
+        </div>
+    )
+}
+export default LeaveManagementPage
